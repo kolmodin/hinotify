d6cdcf649be4ee1dfd76d8e9b861498e385e13fd
Lennart Kolmodin <kolmodin@dtek.chalmers.se>
2006-10-04 21:32:44 +0400

    Cleanup of simple test

--- tests/simple/simple.hs
+++ tests/simple/simple.hs
@@ -9,9 +9,8 @@ main = do
     inotify <- inotify_init
     print inotify
     home <- getHomeDirectory
-    wd <- inotify_add_watch inotify [Open,Close,Access,Modify,Move] home (\x -> putStrLn "got event:" >> print x)
+    wd <- inotify_add_watch inotify [Open,Close,Access,Modify,Move] home print
     print wd
     putStrLn "Listens to your home directory. Hit enter to terminate."
     getLine
     inotify_rm_watch inotify wd
-
