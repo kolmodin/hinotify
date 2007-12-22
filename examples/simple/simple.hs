2bc7a8e871720e4b89ffa1963d69e9b4fa403084
Lennart Kolmodin <kolmodin@gentoo.org>
2007-12-22 19:55:06 +0300

    Update simple example to new API

--- examples/simple/simple.hs
+++ examples/simple/simple.hs
@@ -3,14 +3,15 @@ module Main where
 import System.Directory
 import System.IO
 
-import System.INotify
+import System.INotify as INotify
+
 main :: IO ()
 main = do
-    inotify <- inotify_init
+    inotify <- INotify.init
     print inotify
     home <- getHomeDirectory
-    wd <- inotify_add_watch inotify [Open,Close,Access,Modify,Move] home print
+    wd <- addWatch inotify [Open,Close,Access,Modify,Move] home print
     print wd
     putStrLn "Listens to your home directory. Hit enter to terminate."
     getLine
-    inotify_rm_watch inotify wd
+    removeWatch inotify wd
