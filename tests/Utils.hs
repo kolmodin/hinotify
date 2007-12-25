c1591f11ffe0c1df343f5a8bd3c4cd02bda1d474
Lennart Kolmodin <kolmodin@gentoo.org>
2007-12-25 12:24:09 +0300

    Test utilities

---
+++ tests/Utils.hs
@@ -0,0 +1,55 @@
+module Utils where
+
+import Control.Concurrent.Chan
+import Control.Exception
+
+import System.Directory
+import System.Environment
+import System.Exit
+
+import System.INotify as INotify
+
+testName = do
+    n <- getProgName
+    return (n ++ "-playground")
+
+withTempDir f = do
+    path <- testName
+    bracket
+        ( createDirectory path >> return path )
+        ( removeDirectoryRecursive )
+        ( f )
+
+withWatch inot events path action f =
+    bracket
+        ( addWatch inot events path action )
+        ( removeWatch inot )
+        ( const f )
+
+inTestEnviron events action f = do
+    withTempDir $ \testPath -> do
+        inot <- INotify.init
+        chan <- newChan
+        withWatch inot events testPath (writeChan chan) $ do
+            action testPath
+            events <- getChanContents chan
+            f events
+
+(~=) :: Eq a => [a] -> [a] -> Bool
+[] ~= _ = True
+(x:xs) ~= (y:ys) = x == y && xs ~= ys
+_ ~= _ = False
+
+asMany :: [a] -> [a] -> [a]
+asMany xs ys = take (length xs) ys
+
+explainFailure expected reality = do
+    putStrLn "Expected:"
+    mapM_ print expected
+    putStrLn "But got:"
+    mapM_ print (asMany expected reality)
+    testFailure
+
+testFailure = exitFailure 
+
+testSuccess = exitWith ExitSuccess
